f46bc08a1d245a963c5df5bdf362c03fadaf7e5d
unknown <camilaramirezburbano39@gmail.com>
2024-03-12 23:17:46 -0500

    continuar

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from . import app, db 
-from .models import Consultorio, Medico, Paciente
+from .models import Cita, Consultorio, Medico, Paciente
 from flask import render_template, request 
 
 #crear ruta para ver los listados los medicos 
@@ -19,6 +19,11 @@ def get_all_pacientes():
 def get_all_consultorios():
     consultorios = Consultorio.query.all()
     return render_template("consultorios.html", consultorios=consultorios)
+
+@app.route("/citas")
+def get_all_citas():
+    citas = Cita.query.all()
+    return render_template("citas.html", citas=citas)
 
 #crear ruta tarer medico por id(get)
 @app.route("/medicos/<int:id>")
@@ -70,4 +75,72 @@ def create_medico():
         db.session.add(new_medico)       
         db.session.commit()
         return "medico registrado"
-         
+    
+    #crear ruta para nuevo paciente
+@app.route("/pacientes/create" , methods = ["GET" , "POST"])
+def create_paciente():
+    #mostrar el formulario: metodo GET 
+    if( request.method == "GET"  ):
+        #el usuario ingreso con navegador con http://localhost:5000/medico
+        return render_template("paciente_form.html")
+  
+    #####
+    ## Cuando el usuario presione el boton guardar
+    # los datos del formulario viajan al servidor 
+    # utilizar el boton POST
+    elif(request.method == "POST"):
+        #cuando se presione "guardar"
+        #crear un objeto de tipo pacinete
+        new_paciente = Paciente(nombre = request.form["nombre"],
+                            apellidos = request.form["apellidos"],
+                            tipo_identificacion = request.form["ti"],
+                            numero_identificacion =request.form["ni"],
+                            altura = request.form["al"],
+                            tipo_sangre = request.form["ts"]
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_paciente)       
+        db.session.commit()
+        return "paciente registrado"
+    
+    
+@app.route("/consultorios/create" , methods = ["GET" , "POST"])
+def create_consultorio():
+    #mostrar el formulario: metodo GET 
+    if( request.method == "GET"  ):
+        #el usuario ingreso con navegador con http://localhost:5000/medico
+        return render_template("consultorio_form.html")
+   
+    elif(request.method == "POST"):
+        new_consultorio = Consultorio(numero = request.form["nu"])
+        
+        db.session.add(new_consultorio)       
+        db.session.commit()
+        return "consultorio registrado"
+    
+        #crear ruta para nuevo cita
+    
+@app.route("/citas/create" , methods = ["GET" , "POST"])
+def create_cita():
+    #mostrar el formulario: metodo GET 
+    if( request.method == "GET"  ):
+        #el usuario ingreso con navegador con http://localhost:5000/medico
+        return render_template("cita_form.html")
+  
+    #####
+    ## Cuando el usuario presione el boton guardar
+    # los datos del formulario viajan al servidor 
+    # utilizar el boton POST
+    elif(request.method == "POST"):
+        #cuando se presione "guardar"
+        #crear un objeto de tipo pacinete
+        new_cita = Cita(fecha = request.form["fe"],
+                            pacinete = request.form["pa"],
+                            medico = request.form["me"],
+                            consultorio =request.form["co"],
+                            valor = request.form["va"],
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_cita)       
+        db.session.commit()
+        return "cita registrado"
